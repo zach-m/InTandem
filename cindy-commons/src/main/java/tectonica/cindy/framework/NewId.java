cc130e75308e0204fbc51101f9dbbbf2612adcf3
zach <zach@tokenads.com>
2014-07-10 12:27:20 +0300

    added ID generator

---
+++ cindy-commons/src/main/java/tectonica/cindy/framework/NewId.java
@@ -0,0 +1,27 @@
+package tectonica.cindy.framework;
+
+import java.util.Random;
+
+public class NewId
+{
+	private static Random rand = new Random();
+
+	/**
+	 * returns a so-called Time-UUID, i.e. a UUID that guarantees no conflicts at the following probabilities:
+	 * <ul>
+	 * <li>99.9999973% (or 1 - 2.7e-8) if called 1,000,000 times in every millisecond
+	 * <li>value of (1 - 2.7e-14) if called 1,000 times in every millisecond (i.e. every microsecond)
+	 * <li>either way, UUIDs within different milliseconds have 0% probability of conflict
+	 * </ul>
+	 * The resulting UUID when used in sort retains order of creation
+	 */
+	public static String generate()
+	{
+		String timePart = Long.toHexString(System.currentTimeMillis());
+		// NOTE: all dates between 2004 and and 2527 are taking up 11 hex digits
+
+		String randomPart = Long.toHexString(rand.nextLong());
+
+		return timePart.concat(randomPart);
+	}
+}
